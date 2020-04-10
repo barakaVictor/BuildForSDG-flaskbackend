a369ccf20093586732dc257eaed23684311f62b0
Victor Baraka <barakavictor62@gmail.com>
2020-04-10 23:12:41 +0300

    refactor(api routes): implement class based views by subclassing flask.view.MethodView for better http method call handling

--- covid19estimator/api/routes.py
+++ covid19estimator/api/routes.py
@@ -1,23 +1,28 @@
 import json
 from flask import request, jsonify, Response, render_template, abort
+from flask.views import MethodView
 from covid19estimator.api import api_bp
 from covid19estimator.utilities.jsonxml import json_to_xml
 from covid19estimator.utilities.estimator import estimator
 from covid19estimator.utilities.logsgetter import get_logs
 
+class CovidImpactEstimator(MethodView):
 
-@api_bp.route("/v1/on-covid-19", defaults={"output_format": None}, methods=('POST', 'GET'))
-@api_bp.route("/v1/on-covid-19/<string:output_format>", methods=('POST', 'GET'))
-def estimate(output_format):
-    if request.method == "POST":
+    accepted_formats = ['json', 'xml']
+
+    def get(self, resource):
+        if resource and resource == 'logs':
+            return get_logs()
+        else:
+            abort(404, description="Resource not found")
+
+    def post(self, data_format):
         output = estimator(request.get_json(force=True))
-        if output_format and output_format == 'xml':
+        if data_format and data_format == "xml" :
             return Response(json_to_xml(output), mimetype='application/xml')
         return jsonify(output)
-    
-    elif output_format and output_format == 'logs':
-        return get_logs()
-    else:
-        abort(404, description="Resource not found")
 
-
+view = CovidImpactEstimator.as_view('estimator')
+api_bp.add_url_rule("/v1/on-covid-19/<string:resource>", view_func=view, methods=['GET'])
+api_bp.add_url_rule("/v1/on-covid-19", defaults={'data_format': None }, view_func=view, methods=['POST'])
+api_bp.add_url_rule("/v1/on-covid-19/<string:data_format>", view_func=view, methods=['POST'])
