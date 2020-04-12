f1cfdfb27b717e3ecbc28ff62b82a6681b76dc00
Victor Baraka <barakavictor62@gmail.com>
2020-04-12 19:45:53 +0300

    fix(logs): remove log files from git tracking

--- startapp.py
+++ startapp.py
@@ -1,3 +1,9 @@
+"""
+The purpose of this script is to launch the
+application in development mode. It should not
+be used launch the application in any production
+environment
+"""
 from covid19estimator import create_app
 
 app = create_app()
