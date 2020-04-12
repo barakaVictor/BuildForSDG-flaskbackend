095262727d7f56a6836dca6185258a766e3f549b
Victor Baraka <barakavictor62@gmail.com>
2020-04-12 19:47:06 +0300

    fix(settings): switch to production settings

--- covid19estimator/settings/__init__.py
+++ covid19estimator/settings/__init__.py
@@ -1,6 +1,6 @@
 import logging
 try:
-    from covid19estimator.settings.development import *
-    #from covid19estimator.settings.production import *
+    #from covid19estimator.settings.development import *
+    from covid19estimator.settings.production import *
 except ImportError as error:
     logging.error(error)
