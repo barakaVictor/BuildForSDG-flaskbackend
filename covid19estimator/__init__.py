007597befcfadbdb4119d7b62091e877de7879b4
Victor Baraka <barakavictor62@gmail.com>
2020-04-12 19:24:05 +0300

    fix(request context logging): register before_request and after_request hooks in create_app() factory to log request context information

--- covid19estimator/__init__.py
+++ covid19estimator/__init__.py
@@ -13,4 +13,16 @@ def create_app(config='covid19estimator.settings'):
     from .main import main_bp
     app.register_blueprint(main_bp)
 
+    @app.before_request
+    def start_request_timer():
+        g.start = time.time()
+
+    @app.after_request
+    def log_request_info(response):
+        if request.path in app.config.get("LOG_ROUTES"):
+            diff = int((time.time() - g.start) * 1000)
+            log = f"{request.method} {request.path} {response.status_code} {diff} ms"
+            app.logger.info(log)
+        return response
+
     return app
--- startapp.py
+++ startapp.py
@@ -4,18 +4,6 @@ from flask import g, request
 from covid19estimator import create_app
 
 app = create_app()
- 
-@app.before_request
-def start_request_timer():
-    g.start = time.time()
-
-@app.after_request
-def log_request_info(response):
-    if request.path in app.config.get("LOG_ROUTES"):
-        diff = int((time.time() - g.start) * 1000)
-        log = f"{request.method} {request.path} {response.status_code} {diff} ms"
-        app.logger.info(log)
-    return response
 
 if __name__ == "__main__":
     app.run()
